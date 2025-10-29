34269413da1d7c2bd60b863505e0c09024a6940c
Mu <musabojolowo2006@gmail.com>
2025-10-29 19:51:59 +0100

    Update cms-init.js

--- admin/cms-init.js
+++ admin/cms-init.js
@@ -1,22 +1,8 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   if (window.CMS) {
     console.log("✅ Netlify CMS loaded successfully");
     window.CMS.init();
-  } else if (window.NetlifyCmsApp) {
-    console.log("✅ Netlify CMS App loaded successfully");
-    window.NetlifyCmsApp.init();
   } else {
-    console.error("❌ Netlify CMS failed to load. Retrying...");
-
-    // Try again after a short delay
-    setTimeout(() => {
-      if (window.CMS) {
-        window.CMS.init();
-      } else if (window.NetlifyCmsApp) {
-        window.NetlifyCmsApp.init();
-      } else {
-        console.error("⚠️ Still couldn't find Netlify CMS.");
-      }
-    }, 2000);
+    console.error("❌ Netlify CMS failed to load.");
   }
 });
