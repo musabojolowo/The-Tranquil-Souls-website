91ccd6ad8976d5658f4fd0d415f7566cf295c970
Mu <musabojolowo2006@gmail.com>
2025-10-29 19:53:57 +0100

    Delete admin/cms-init.js

--- admin/cms-init.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function () {
-  if (window.CMS) {
-    console.log("✅ Netlify CMS loaded successfully");
-    window.CMS.init();
-  } else {
-    console.error("❌ Netlify CMS failed to load.");
-  }
-});
